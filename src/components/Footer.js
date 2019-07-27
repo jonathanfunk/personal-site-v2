3f4c13cd0aca87d04944426f0a00badf5ab60ca8
Jonathan Funk <jonfunk88@hotmail.com>
2019-07-27 09:18:55 -0700

    social media icons added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
-import React from "react"
+import React from "react";
+import LinkedinIcon from "./icons/LinkedinIcon";
+import GithubIcon from "./icons/GithubIcon";
+import EnvelopeIcon from "./icons/EnvelopeIcon";
 
 const Footer = () => {
   return (
@@ -9,9 +12,9 @@ const Footer = () => {
         </div>
         <div className="social-icons">
           <ul>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
+            <li><a href={`https://linkedin.com/in/jonathan-funk/`} target="_blank"><LinkedinIcon /></a></li>
+            <li><a href={`https://github.com/jonathanfunk`} target="_blank"><GithubIcon /></a></li>
+            <li><a href={`mailto:jonfunk88@hotmail.com`}><EnvelopeIcon /></a></li>
           </ul>
         </div>
       </div>
